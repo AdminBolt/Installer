5194a47a153a91b46d7bbff583cf1d23eac2a51f
Bozhidar <69676022+bobicloudvision@users.noreply.github.com>
2025-06-11 18:05:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,344 +1,311 @@
 #!/bin/bash
 
-# Check is AdminBolt is already installed
-if [ -d "/usr/local/bolt" ]; then
-echo "AdminBolt is already installed. Exiting..."
-exit 0
-fi
+# Color definitions
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+CYAN="\e[36m"
+BOLD="\e[1m"
+RESET="\e[0m"
+
+# Get terminal width or fallback to 80
+TERM_WIDTH=$(tput cols 2>/dev/null || echo 80)
+LINE=$(printf '%*s' "${TERM_WIDTH}" '' | tr ' ' '-')
+
+# Group and user parameters
+GROUP_NAME="boltweb"
+GROUP_ID="801"
+USER_NAME="boltweb"
+USER_ID="801"
+USER_HOME="/usr/local/bolt"
+USER_SHELL="/sbin/nologin"
 
 # Check if the user is root
-if [[ $EUID -ne 0 ]]; then
-echo "This script must be run as root. Exiting..."
-exit 1
+if [[ ${EUID} -ne 0 ]]; then
+	echo "This script must be run as root. Exiting..."
+	exit 1
 fi
 
 # Check if the user is running a 64-bit system
 if [[ $(uname -m) != "x86_64" ]]; then
-echo "This script must be run on a 64-bit system. Exiting..."
-exit 1
+	echo "This script must be run on a 64-bit system. Exiting..."
+	exit 1
 fi
 
 # Check if the user is running a supported shell
-if [[ $(echo $SHELL) != "/bin/bash" ]]; then
-echo "This script must be run on a system running Bash. Exiting..."
-exit 1
+if [[ $(echo ${SHELL}) != "/bin/bash" ]]; then
+	echo "This script must be run on a system running Bash. Exiting..."
+	exit 1
 fi
 
 # Check if the user is running a supported OS
 if [[ $(uname -s) != "Linux" ]]; then
-echo "This script must be run on a Linux system. Exiting..."
-exit 1
+	echo "This script must be run on a Linux system. Exiting..."
+	exit 1
 fi
 
-# Check if the user is running a supported distro version
-DISTRO_VERSION=$(cat /etc/os-release | grep -w "VERSION_ID" | cut -d "=" -f 2)
-DISTRO_VERSION=${DISTRO_VERSION//\"/} # Remove quotes from version string
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING REPOSITORIES ${RESET}"
+echo -e "${LINE}"
+# System Update and Repositories
+dnf update -y && echo "✅ System updated!" || echo "❌ System update failed!"
+dnf install epel-release -y && echo "✅ EPEL repository installed!" || echo "❌ EPEL repository install failed!"
+dnf config-manager --set-enabled epel && echo "✅ EPEL repository enabled!" || echo "❌ EPEL repository enable failed!"
+dnf config-manager --set-enabled crb && echo "✅ CRB repository enabled!" || echo "❌ CRB repository enable failed!"
 
-DISTRO_NAME=$(cat /etc/os-release | grep -w "NAME" | cut -d "=" -f 2)
-DISTRO_NAME=${DISTRO_NAME//\"/} # Remove quotes from name string
-# Lowercase the distro name
-DISTRO_NAME=$(echo $DISTRO_NAME | tr '[:upper:]' '[:lower:]')
-# replace spaces
-DISTRO_NAME=${DISTRO_NAME// /-}
+wget https://raw.githubusercontent.com/AdminBolt/Install/refs/heads/main/almalinux-9.5/repos/bolt.repo -q && echo "✅ Bolt repository downloaded!" || echo "❌ Bolt repository download failed!"
+mv bolt.repo /etc/yum.repos.d/bolt.repo && echo "✅ Bolt repository installed!" || echo "❌ Bolt repository install failed!"
 
 
-installAlmalinuxNineFour()
-{
-    INSTALL_DIR="/bolt/install"
-
-yum update -y
-dnf -y install sudo wget
-export NON_INT=1
-sudo wget -q -O - http://www.atomicorp.com/installers/atomic | sh
-dnf install epel-release -y
-dnf config-manager --set-enabled epel
-dnf config-manager --set-enabled crb
-yum install -y libsodium libsodium-devel
-
-mkdir -p $INSTALL_DIR
-
-cd $INSTALL_DIR
-
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING PACKAGES ${RESET}"
+echo -e "${LINE}"
+# Install Required Dependencies
 DEPENDENCIES_LIST=(
-    "apg"
-    "openssl"
-    "jq"
-    "curl"
-    "wget"
-    "unzip"
-    "zip"
-    "tar"
-    "mysql-common"
-    "mysql-server"
-    "lsb-release"
-    "gnupg2"
-    "ca-certificates"
-    "apt-transport-https"
-    "supervisor"
+	"sudo"
+	"apg"
+	"openssl"
+	"jq"
+	"curl"
+	"wget"
+	"unzip"
+	"zip"
+	"tar"
+	"gnupg2"
+	"ca-certificates"
+	"supervisor"
+	"libsodium"
+	"libsodium-devel"
+    "bolt-php"
+    "bolt-nginx"
+    "bolt-updater"
 )
-# Check if the dependencies are installed
-for DEPENDENCY in "${DEPENDENCIES_LIST[@]}"; do
-    dnf install -y $DEPENDENCY
+for DEP in "${DEPENDENCIES_LIST[@]}"; do
+    RPM_QUERY=$(rpm -q "$DEP" 2>&1)  # Capture both output and error
+    if [ $? -eq 0 ]; then
+        echo -e "✅ $DEP is already installed: ${RPM_QUERY}"
+    else
+        echo -e "⚠️ $DEP is not installed. rpm output: ${RPM_QUERY}"
+        echo -e "Installing $DEP..."
+        dnf install -y "$DEP"
+    fi
 done
 
-## Start MySQL
-systemctl start mysqld
-systemctl enable mysqld
+# Create boltweb User
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}CREATING SYSTEM USER ${RESET}"
+echo -e "${LINE}"
 
-cat > /etc/profile.d/bolt-greeting.sh <<EOF
-#!/bin/bash
-
-CURRENT_IP=$(hostname -I | awk '{print $1}')
-
-echo "
- Welcome to AdminBolt!
- OS: AlmaLinux 9.4
- You can login at: http://$CURRENT_IP:8443
-"
-EOF
-
-dnf install https://license.adminbolt.com/mirrorlist/almalinux/9/bolt-repo-1.0.0-1.el9.noarch.rpm -y
-
-dnf install -y bolt-php --enablerepo=bolt
-dnf install -y bolt-nginx --enablerepo=bolt
-dnf install -y bolt-updater --enablerepo=bolt
-dnf install -y httpd --enablerepo=bolt
-
-systemctl start httpd
-systemctl enable httpd
-
-BOLT_PHP=/usr/local/bolt/php/bin/php
-ln -s $BOLT_PHP /usr/bin/bolt-php
-HOSTNAME=$(hostname)
-IP_ADDRESS=$(hostname -I | cut -d " " -f 1)
-
-DISTRO_VERSION=$(cat /etc/os-release | grep -w "VERSION_ID" | cut -d "=" -f 2)
-DISTRO_VERSION=${DISTRO_VERSION//\"/} # Remove quotes from version string
-
-DISTRO_NAME=$(cat /etc/os-release | grep -w "NAME" | cut -d "=" -f 2)
-DISTRO_NAME=${DISTRO_NAME//\"/} # Remove quotes from name string
-
-LOG_JSON='{"os": "'$DISTRO_NAME-$DISTRO_VERSION'", "host_name": "'$HOSTNAME'", "ip": "'$IP_ADDRESS'"}'
-
-curl -s https://adminbolt.com/api/bolt-installation-log -X POST -H "Content-Type: application/json" -d "$LOG_JSON"
-
-USE_WEB_SOURCE_FROM_PATH=0
-
-# Parse arguments
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-        --use-web-source-from-path=*) USE_WEB_SOURCE_FROM_PATH="${1#*=}";;
-        --use-web-source-from-path) USE_WEB_SOURCE_FROM_PATH="$2"; shift;;
-    esac
-    shift
-done
-
-if [[ -d USE_WEB_SOURCE_FROM_PATH ]]; then
-    echo "Using web source from path"
-    cp -r $USE_WEB_SOURCE_FROM_PATH /usr/local/bolt/web/
+# Create system group
+if getent group "${GROUP_NAME}" >/dev/null; then
+	echo -e "✅ Group '${GROUP_NAME}' already exists."
 else
-    echo "Downloading web source"
-    wget https://license.adminbolt.com/mirrorlist/any/any/adminbolt-web-stable.zip -O adminbolt-cp.zip -q
-    unzip -qq -o adminbolt-cp.zip -d /usr/local/bolt/web
-    rm -rf adminbolt-cp.zip
+	echo -e "Creating group '${GROUP_NAME}' with GID ${GROUP_ID}..."
+	if groupadd --system --gid "${GROUP_ID}" "${GROUP_NAME}"; then
+		echo -e "✅ Group '${GROUP_NAME}' created."
+	else
+		echo -e "❌ Failed to create group '${GROUP_NAME}'."
+		exit 1
+	fi
 fi
 
-# Check dir exists
-if [ ! -d "/usr/local/bolt/web" ]; then
-  echo "AdminBolt directory not found."
-  exit 1
-fi
-
-chmod 711 /home
-chmod -R 750 /usr/local/bolt
-
-ln -s /usr/local/bolt/web/bolt-shell.sh /usr/bin/bolt-shell
-chmod +x /usr/local/bolt/web/bolt-shell.sh
-
-ln -s /usr/local/bolt/web/bolt-cli.sh /usr/bin/bolt-cli
-chmod +x /usr/local/bolt/web/bolt-cli.sh
-
-mkdir -p /usr/local/bolt/ssl
-cp /usr/local/bolt/web/server/ssl/bolt.crt /usr/local/bolt/ssl/bolt.crt
-cp /usr/local/bolt/web/server/ssl/bolt.key /usr/local/bolt/ssl/bolt.key
-cp /usr/local/bolt/web/server/ssl/bolt.chain /usr/local/bolt/ssl/bolt.chain
-
-
-# Go to web directory
-cd /usr/local/bolt/web
-
-chmod -R o+w /usr/local/bolt/web/storage/
-chmod -R o+w /usr/local/bolt/web/bootstrap/cache/
-
-rm -rf /usr/local/bolt/php/lib/php.ini
-ln -s /usr/local/bolt/web/server/php/php.ini /usr/local/bolt/php/lib/php.ini
-
-rm -rf /usr/local/bolt/php/etc/php-fpm.conf
-ln -s /usr/local/bolt/web/server/php/php-fpm.conf /usr/local/bolt/php/etc/php-fpm.conf
-
-rm -rf /usr/local/bolt/nginx/conf/nginx.conf
-ln -s /usr/local/bolt/web/server/nginx/nginx.conf /usr/local/bolt/nginx/conf/nginx.conf
-
-
-service bolt start
-
-bolt-php /usr/local/bolt/web/artisan bolt:install-core
-
-}
-
-installAlmalinuxNineFive()
-{
-    INSTALL_DIR="/bolt/install"
-
-yum update -y
-dnf -y install sudo wget
-export NON_INT=1
-sudo wget -q -O - http://www.atomicorp.com/installers/atomic | sh
-dnf install epel-release -y
-dnf config-manager --set-enabled epel
-dnf config-manager --set-enabled crb
-yum install -y libsodium libsodium-devel
-
-mkdir -p $INSTALL_DIR
-
-cd $INSTALL_DIR
-
-DEPENDENCIES_LIST=(
-    "apg"
-    "openssl"
-    "jq"
-    "curl"
-    "wget"
-    "unzip"
-    "zip"
-    "tar"
-    "mysql-common"
-    "mysql-server"
-    "lsb-release"
-    "gnupg2"
-    "ca-certificates"
-    "apt-transport-https"
-    "supervisor"
-)
-# Check if the dependencies are installed
-for DEPENDENCY in "${DEPENDENCIES_LIST[@]}"; do
-    dnf install -y $DEPENDENCY
-done
-
-## Start MySQL
-systemctl start mysqld
-systemctl enable mysqld
-
-
-cat > /etc/profile.d/bolt-greeting.sh <<EOF
-#!/bin/bash
-
-CURRENT_IP=$(hostname -I | awk '{print $1}')
-
-echo "
- Welcome to AdminBolt!
- OS: AlmaLinux 9.5
- You can login at: http://$CURRENT_IP:8443
-"
-EOF
-
-dnf install https://license.adminbolt.com/mirrorlist/almalinux/9/bolt-repo-1.0.0-1.el9.noarch.rpm -y
-
-
-dnf install -y bolt-php --enablerepo=bolt
-dnf install -y bolt-nginx --enablerepo=bolt
-dnf install -y bolt-updater --enablerepo=bolt
-dnf install -y httpd --enablerepo=bolt
-
-systemctl start httpd
-systemctl enable httpd
-
-BOLT_PHP=/usr/local/bolt/php/bin/php
-ln -s $BOLT_PHP /usr/bin/bolt-php
-HOSTNAME=$(hostname)
-IP_ADDRESS=$(hostname -I | cut -d " " -f 1)
-
-DISTRO_VERSION=$(cat /etc/os-release | grep -w "VERSION_ID" | cut -d "=" -f 2)
-DISTRO_VERSION=${DISTRO_VERSION//\"/} # Remove quotes from version string
-
-DISTRO_NAME=$(cat /etc/os-release | grep -w "NAME" | cut -d "=" -f 2)
-DISTRO_NAME=${DISTRO_NAME//\"/} # Remove quotes from name string
-
-LOG_JSON='{"os": "'$DISTRO_NAME-$DISTRO_VERSION'", "host_name": "'$HOSTNAME'", "ip": "'$IP_ADDRESS'"}'
-
-curl -s https://adminbolt.com/api/bolt-installation-log -X POST -H "Content-Type: application/json" -d "$LOG_JSON"
-
-USE_WEB_SOURCE_FROM_PATH=0
-
-# Parse arguments
-while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-        --use-web-source-from-path=*) USE_WEB_SOURCE_FROM_PATH="${1#*=}";;
-        --use-web-source-from-path) USE_WEB_SOURCE_FROM_PATH="$2"; shift;;
-    esac
-    shift
-done
-
-if [[ -d USE_WEB_SOURCE_FROM_PATH ]]; then
-    echo "Using web source from path"
-    cp -r $USE_WEB_SOURCE_FROM_PATH /usr/local/bolt/web/
+# Create system user
+if id "${USER_NAME}" >/dev/null 2>&1; then
+	echo -e "✅ User '${USER_NAME}' already exists."
 else
-    echo "Downloading web source"
-    wget https://license.adminbolt.com/mirrorlist/any/any/adminbolt-web-stable.zip -O adminbolt-cp.zip -q
-    unzip -qq -o adminbolt-cp.zip -d /usr/local/bolt/web
-    rm -rf adminbolt-cp.zip
+	echo -e "Creating user '${USER_NAME}' with UID ${USER_ID}..."
+	if useradd --uid "${USER_ID}" --gid "${GROUP_ID}" --home "${USER_HOME}" --shell "${USER_SHELL}" "${USER_NAME}"; then
+		echo -e "✅ User '${USER_NAME}' created."
+	else
+		echo -e "❌ Failed to create user '${USER_NAME}'."
+		exit 1
+	fi
 fi
 
-# Check dir exists
-if [ ! -d "/usr/local/bolt/web" ]; then
-  echo "AdminBolt directory not found."
-  exit 1
-fi
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING BOLT REPOSITORIES ${RESET}"
+echo -e "${LINE}"
+# Atomic Repo (optional tools)
+# sudo wget -q -O - http://www.atomicorp.com/installers/atomic | sh
 
-chmod 711 /home
-chmod -R 750 /usr/local/bolt
+# Add AdminBolt Greeting and Repos
+wget https://raw.githubusercontent.com/AdminBolt/Install/refs/heads/main/almalinux-9.5/greeting.sh -q && echo "✅ Greeting text downloaded!" || echo "❌ Greeting text download failed!"
+mv greeting.sh /etc/profile.d/bolt-greeting.sh && echo "✅ Greeting text installed!" || echo "❌ Greeting text install failed!"
 
-ln -s /usr/local/bolt/web/bolt-shell.sh /usr/bin/bolt-shell
-chmod +x /usr/local/bolt/web/bolt-shell.sh
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING ADMINBOLT PANEL ${RESET}"
+echo -e "${LINE}"
 
-ln -s /usr/local/bolt/web/bolt-cli.sh /usr/bin/bolt-cli
-chmod +x /usr/local/bolt/web/bolt-cli.sh
-
-mkdir -p /usr/local/bolt/ssl
-cp /usr/local/bolt/web/server/ssl/bolt.crt /usr/local/bolt/ssl/bolt.crt
-cp /usr/local/bolt/web/server/ssl/bolt.key /usr/local/bolt/ssl/bolt.key
-cp /usr/local/bolt/web/server/ssl/bolt.chain /usr/local/bolt/ssl/bolt.chain
-
-
-# Go to web directory
-cd /usr/local/bolt/web
-
-chmod -R o+w /usr/local/bolt/web/storage/
-chmod -R o+w /usr/local/bolt/web/bootstrap/cache/
-
-rm -rf /usr/local/bolt/php/lib/php.ini
-ln -s /usr/local/bolt/web/server/php/php.ini /usr/local/bolt/php/lib/php.ini
-
-rm -rf /usr/local/bolt/php/etc/php-fpm.conf
-ln -s /usr/local/bolt/web/server/php/php-fpm.conf /usr/local/bolt/php/etc/php-fpm.conf
-
-rm -rf /usr/local/bolt/nginx/conf/nginx.conf
-ln -s /usr/local/bolt/web/server/nginx/nginx.conf /usr/local/bolt/nginx/conf/nginx.conf
-
-
-service bolt start
-
-bolt-php /usr/local/bolt/web/artisan bolt:install-core
-
-}
-
-if [[ $DISTRO_NAME == "almalinux" && $DISTRO_VERSION == "9.5" ]]; then
-    echo "Supported OS detected: $DISTRO_NAME $DISTRO_VERSION"
-    installAlmalinuxNineFive
-elif [[ $DISTRO_NAME == "almalinux" && $DISTRO_VERSION == "9.4" ]]; then
-    echo "Supported OS detected: $DISTRO_NAME $DISTRO_VERSION"
-    installAlmalinuxNineFour
+# Create folder if it doesn't exist
+if [ ! -d /usr/local/bolt ]; then
+    mkdir -p /usr/local/bolt
+    chown boltweb:boltweb /usr/local/bolt
+    chmod 750 /usr/local/bolt
+    dnf reinstall -y bolt-php bolt-nginx bolt-updater
 else
-    echo "Unsupported OS detected: $DISTRO_NAME $DISTRO_VERSION. Exiting..."
+    echo "✅ /usr/local/bolt exists..."
+fi
+
+# Find latest stable version of AdminBolt Panel
+GET_LATEST_ADMINBOLT_VERSION=$(curl -s "https://license.adminbolt.com/api/get-latest-version-of-package?os=any&os_version=any&name=adminbolt-web&branch=stable")
+ADMINBOLT_VERSION=$(echo "$GET_LATEST_ADMINBOLT_VERSION" | jq -r '.latest_version')
+DOWNLOAD_LINK=$(echo "$GET_LATEST_ADMINBOLT_VERSION" | jq -r '.package.download_link')
+if [[ -z "$ADMINBOLT_VERSION" || -z "$DOWNLOAD_LINK" ]]; then
+    echo -e "${RED}❌ Failed to retrieve the latest version or download link. Exiting...${RESET}"
     exit 1
 fi
+
+echo -e "✅ Latest AdminBolt Panel version: ${ADMINBOLT_VERSION}"
+
+# Download and Extract AdminBolt Panel
+{
+    wget $DOWNLOAD_LINK -O adminbolt-cp.zip -q
+} && echo "✅ AdminBolt Panel downloaded!" || echo "❌ AdminBolt Panel download failed!"
+
+{
+unzip -qq -o adminbolt-cp.zip -d /usr/local/bolt/web
+} && echo "✅ AdminBolt Panel deployed!" || echo "❌ AdminBolt Panel deploy failed!"
+
+rm -rf adminbolt-cp.zip
+
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING ADMINBOLT PANEL ${RESET}"
+echo -e "${LINE}"
+
+# Install SSL Certificates
+mkdir -p /usr/local/bolt/ssl
+{
+    cp /usr/local/bolt/web/server/ssl/bolt.crt /usr/local/bolt/ssl/bolt.crt
+    cp /usr/local/bolt/web/server/ssl/bolt.key /usr/local/bolt/ssl/bolt.key
+    cp /usr/local/bolt/web/server/ssl/bolt.chain /usr/local/bolt/ssl/bolt.chain
+} && echo "✅ Installed SSL Certificates!" || echo "❌ Install SSL Certificates failed!"
+
+# Apply NGINX & PHP Configuration
+{
+    mkdir -p /usr/local/bolt/nginx/conf
+    cp /usr/local/bolt/web/server/nginx/nginx.conf /usr/local/bolt/nginx/conf/nginx.conf
+
+    mkdir -p /usr/local/bolt/php/etc
+    cp /usr/local/bolt/web/server/php/php-fpm.conf /usr/local/bolt/php/etc/php-fpm.conf
+
+    mkdir -p /usr/local/bolt/php/lib
+    cp /usr/local/bolt/web/server/php/php.ini /usr/local/bolt/php/lib/php.ini
+} && echo "✅ NGINX & PHP Configuration applied!" || echo "❌ NGINX & PHP Configuration failed!"
+
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}SET OWNERSHIP AND PERMISSIONS ${RESET}"
+echo -e "${LINE}"
+
+# Set Ownership and Permissions
+{
+    # Ensure the boltweb user owns the /usr/local/bolt directory
+    chown -R boltweb:boltweb /usr/local/bolt
+    chmod 750 /usr/local/bolt
+} && echo "✅ Ownership and permissions set for /usr/local/bolt!" || echo "❌ Ownership and permissions failed!"
+
+{
+    # Set 750 for directories
+    find /usr/local/bolt -type d -exec chmod 750 {} \;
+    # Set 640 for regular files
+    find /usr/local/bolt -type f -exec chmod 640 {} \;
+} && echo "✅ Permissions set for directories and files!" || echo "❌ Permissions set for directories and files failed!"
+
+{
+    # Set +x for scripts and artisan
+    chmod +x /usr/local/bolt/php/sbin/bolt-php-fpm
+    chmod +x /usr/local/bolt/nginx/sbin/bolt-nginx
+    find /usr/local/bolt -type f -name "*.sh" -exec chmod +x {} \;
+    find /usr/local/bolt -type f -name "*artisan*" -exec chmod +x {} \;
+} && echo "✅ Permissions set for scripts and artisan!" || echo "❌ Permissions set for scripts and artisan failed!"
+
+{
+    # Set Web Permissions
+    chmod -R o+w /usr/local/bolt/web/storage/
+    chmod -R o+w /usr/local/bolt/web/bootstrap/cache/
+} && echo "✅ Permissions set for webapp work directories!" || echo "❌ Permissions set for webapp directories failed!"
+
+{
+    # Set Permissions and Shell Tools
+    ln -sf /usr/local/bolt/php/bin/php /usr/bin/bolt-php
+    chmod +x /usr/local/bolt/php/bin/php
+    ln -sf /usr/local/bolt/web/bolt-shell.sh /usr/bin/bolt-shell
+    chmod +x /usr/local/bolt/web/bolt-shell.sh
+    ln -sf /usr/local/bolt/web/bolt-cli.sh /usr/bin/bolt-cli
+    chmod +x /usr/local/bolt/web/bolt-cli.sh
+} && echo "✅ Permissions set for Shell Tools!" || echo "❌ Permissions set for Shell Tools failed!"
+
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}SENDING INSTALLATION STATS TO ADMINBOLT API... ${RESET}"
+echo -e "${LINE}"
+
+# Log OS Info to AdminBolt API
+HOSTNAME=$(hostname)
+IP_ADDRESS=$(hostname -I | cut -d " " -f 1)
+DISTRO_VERSION=$(grep -w "VERSION_ID" /etc/os-release | cut -d '"' -f 2)
+DISTRO_NAME=$(grep -w "NAME" /etc/os-release | cut -d '"' -f 2)
+LOG_JSON='{"os": "'$DISTRO_NAME-$DISTRO_VERSION'", "host_name": "'$HOSTNAME'", "ip": "'$IP_ADDRESS'"}'
+RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" https://adminbolt.com/api/bolt-installation-log \
+  -X POST -H "Content-Type: application/json" -d "$LOG_JSON")
+
+if [[ "${RESPONSE}" = "200" ]]; then
+    echo -e "✅ Installation log sent successfully."
+else
+    echo -e "❌ Failed to send log to AdminBolt API (HTTP ${RESPONSE})."
+fi
+
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}START BOLT SERVICES ${RESET}"
+echo -e "${LINE}"
+{
+    # Start Bolt Services
+    service bolt start
+} && echo "✅ Bolt Services started!" || echo "❌ Bolt Services failed to start!"
+
+echo -e ""
+echo -e "${LINE}"
+echo -e "${BOLD}INSTALLING HOSTING SERVICES... ${RESET}"
+echo -e "${LINE}"
+
+# Install all required packages in one go
+dnf install -y \
+  yum-utils \
+  supervisor \
+  bolt-securebox \
+  git \
+  fail2ban \
+  traceroute \
+  vsftpd \
+  pam-devel \
+  libdb-utils \
+  gcc \
+  make \
+  firewalld \
+  sscg \
+  mod_maxminddb \
+  mod_security \
+  mod_ssl \
+  mod_suphp \
+  php84 \
+  php84-php-fpm \
+  goaccess \
+  GeoIP \
+  GeoIP-devel \
+  GeoIP-data \
+  httpd --enablerepo=bolt
+
+dnf update -y
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
+
+chmod +x /usr/local/bolt/web/bolt-install.sh
+/usr/local/bolt/web/bolt-install.sh
